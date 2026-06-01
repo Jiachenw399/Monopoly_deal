bf487e28bc47f798c193155aeefc120f64a67a27
24372224 <2716653751@qq.com>
2026-06-02 02:45:42 +0800

    Create Hidden Card Class

---
+++ src/main/java/model/HiddenCard.java
@@ -0,0 +1,7 @@
+package model;
+public class HiddenCard extends Card {
+    // Creates a hidden card placeholder for another player's private hand.
+    public HiddenCard() {
+        setValue(0);
+    }
+}
